faf5c6d97e5a0dcbb172e4f8626ee239ad8f0377
M-Duyen <22721461.duyen@student.iuh.edu.vn>
2025-04-27 11:54:04 +0700

    trang orderHistory

--- src/main/java/ui/main/AddPromotion.java
+++ src/main/java/ui/main/AddPromotion.java
@@ -29,6 +29,7 @@ import java.rmi.RemoteException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
+import java.util.List;
 
 public class AddPromotion extends JPanel {
 
@@ -431,7 +432,7 @@ public class AddPromotion extends JPanel {
 
     private void searchPromotion(Object cri_type, Object crt_sta) throws RemoteException {
 
-        ArrayList<Promotion> promotionList = new ArrayList<>();
+        List<Promotion> promotionList = new ArrayList<>();
 
         if (cri_type == null && crt_sta != null) {
             if (crt_sta.equals("Đang áp dụng")) {
@@ -472,7 +473,7 @@ public class AddPromotion extends JPanel {
     }
 
 
-    private void loadTable(ArrayList<Promotion> promotionList) {
+    private void loadTable(List<Promotion> promotionList) {
         DefaultTableModel model = (DefaultTableModel) tableKhuyenMai.getModel();
         model.setRowCount(0);
         for (Promotion promotion : promotionList) {
